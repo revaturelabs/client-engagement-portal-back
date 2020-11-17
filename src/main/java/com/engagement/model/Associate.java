9eea1a9ee7b82c325cd6ac2b77c049b59493719c
matthewjunglee <71738797+matthewjunglee@users.noreply.github.com>
2020-11-17 13:53:41 -0800

    associate id primary key

--- src/main/java/com/engagement/model/Associate.java
+++ src/main/java/com/engagement/model/Associate.java
@@ -2,6 +2,8 @@ package com.engagement.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
@@ -15,6 +17,10 @@ import lombok.NoArgsConstructor;
 public class Associate {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int assocId;
+	
+	@Column(nullable = false, unique = true)
 	private String salesForceId;
 
 	@Column(nullable = false, unique = true)
