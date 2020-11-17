78fc8baa1ab8de89370b995dffb5bb358b95df6f
KELSEY Iafrate <kiafrate13@gmail.com>
2020-11-17 16:31:43 -0600

    forgot to save employee

--- src/main/java/com/engagement/model/Employee.java
+++ src/main/java/com/engagement/model/Employee.java
@@ -1,10 +1,12 @@
 package com.engagement.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
 	
 	private String email;
