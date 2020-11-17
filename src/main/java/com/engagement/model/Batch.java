03fa3a2508ae23877c51b5ba60ea63a19111cc2b
KELSEY Iafrate <kiafrate13@gmail.com>
2020-11-17 16:36:01 -0600

    Adds EmployeeAssignment model and makes assignment objects lists of assignments

--- src/main/java/com/engagement/model/Batch.java
+++ src/main/java/com/engagement/model/Batch.java
@@ -1,5 +1,7 @@
 package com.engagement.model;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,6 +20,6 @@ public class Batch {
     private int goodGrade;
     private int passingGrade;
     private int currentWeek;
-    private Object employeeAssignments[];
-    private Object associateAssignments[];
+    private List<EmployeeAssignment> employeeAssignments;
+    private List<AssociateAssignment> associateAssignments;
 }
