2c77e07a8498e5641bec1e17dead3d94b956125a
Matt <mkhartm@gmail.com>
2020-11-17 14:33:33 -0600

    iterating models

--- src/main/java/com/engagement/model/Client.java
+++ src/main/java/com/engagement/model/Client.java
@@ -38,6 +38,8 @@ public class Client {
 	
 	private String phoneNumber;
 	
+	
+	
 
 	
 //	@OneToMany(fetch = FetchType.LAZY)
--- src/main/java/com/engagement/model/ClientBatch.java
+++ src/main/java/com/engagement/model/ClientBatch.java
@@ -1,7 +1,33 @@
 package com.engagement.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class ClientBatch {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int clientBatchId;
 	
+	
+	private int batchId;
+	
+	@ManyToOne
+	@JoinColumn
+	private Client client;
 
 }
