57c91fe4ac6665fa1645cc5804d7e9c8ce5662d2
Matt <mkhartm@gmail.com>
2020-11-18 15:22:06 -0600

    login controller_service

---
+++ src/main/java/com/engagement/controller/LoginController.java
@@ -0,0 +1,40 @@
+package com.engagement.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.engagement.model.Admin;
+import com.engagement.model.Client;
+import com.engagement.service.LoginService;
+
+@Controller
+@CrossOrigin
+@RequestMapping("/login")
+public class LoginController {
+
+	public LoginService ls;
+	
+	@Autowired
+	public LoginController(LoginService ls) {
+		super();
+		this.ls = ls;
+	}
+
+	@PostMapping("/admin")
+	public @ResponseBody Admin loginadmin(@RequestBody String email) {
+		return ls.getAdmin(email);
+	}
+	
+	@PostMapping("/client")
+	public @ResponseBody Client loginclient(@RequestBody String email) {
+		return ls.getClient(email);
+	}
+	
+	
+
+
+
+}
--- src/main/java/com/engagement/repo/ClientRepo.java
+++ src/main/java/com/engagement/repo/ClientRepo.java
@@ -1,5 +1,7 @@
 package com.engagement.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,7 @@ import com.engagement.model.Client;
 @Repository
 public interface ClientRepo extends JpaRepository<Client, Integer> {
 	
-
+	public boolean deleteByEmail(String email);
+	
+	public Client findByEmail(String email);
 }
