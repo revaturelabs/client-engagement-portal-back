0c5996db2b83d4cb14d27b5ed2afd749a7cdd62d
enochcho <enochcho97@hotmail.com>
2020-11-18 14:27:24 -0500

    added javadocs for client repo

--- src/main/java/com/engagement/repo/ClientRepo.java
+++ src/main/java/com/engagement/repo/ClientRepo.java
@@ -6,9 +6,23 @@ import org.springframework.stereotype.Repository;
 
 import com.engagement.model.Client;
 
+
+/**
+ * This is the repo interface for the Clients. 
+ *  Methods like findAll, save, delete, etc are all provided by the JpaRepository hierarchy
+ * @author enoch cho
+ * 
+ *
+ */
 @Repository
 public interface ClientRepo extends JpaRepository<Client, Integer> {
 	
+	/**
+	 * Returns the specific client with the matching id
+	 * @param id
+	 * The int id of the client 
+	 * @return Returns a Client
+	 * 
+	 */
 	Client findByClientId(int id);
-	Client findByUsername(String username);
 }
--- src/test/java/com/engagement/controller/AdminControllerTest.java
+++ src/test/java/com/engagement/controller/AdminControllerTest.java
@@ -1,22 +1,8 @@
 package com.engagement.controller;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import com.engagement.model.Admin;
-import com.engagement.service.AdminService;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(AdminController.class)
