87e301e139aa1df1b433f9588075b74b480bf3be
brookewursten <brooke.wursten@gmail.com>
2020-11-18 10:03:32 -0800

    fixed admin model and all the errors it created

--- src/test/java/com/engagement/controller/AdminControllerTest.java
+++ src/test/java/com/engagement/controller/AdminControllerTest.java
@@ -1,14 +1,51 @@
 package com.engagement.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.engagement.model.Admin;
+import com.engagement.service.AdminService;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(AdminController.class)
 class AdminControllerTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
+	
+	
+	
+//	@Autowired
+//    private MockMvc mvc;
+//	
+//	
+//	@MockBean
+//	private AdminService as;
+//	
+//	private AdminController ac;
+//	
+//	
+//	
+//	private String mockAdminJson = "{'admin':[{'adminId':1 ,'email':'a@a.net','firstName':'admin',lastName':'adminson'}]}";
+//
+//	
+//
+//	@Test
+//	void testCreateNewAdmin() {
+//		
+//		mvc.perform( MockMvcRequestBuilders.post("admin/new")
+//			      .contentType(MediaType.APPLICATION_JSON).andExpect(MockMvcResultMatchers.status().isOk())
+//		            .andExpect(content().json(mockAdminJson));
+//
+//	}
+//
 }
