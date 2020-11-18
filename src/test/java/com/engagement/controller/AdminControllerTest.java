063ea64cdd797696f6b53d3ee02073a48204fd6a
KELSEY Iafrate <kiafrate13@gmail.com>
2020-11-18 15:24:48 -0600

    We got some tests working!

--- src/test/java/com/engagement/controller/AdminControllerTest.java
+++ src/test/java/com/engagement/controller/AdminControllerTest.java
@@ -5,49 +5,68 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.engagement.model.Admin;
 import com.engagement.service.AdminService;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+//@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@WebMvcTest(AdminController.class)
 class AdminControllerTest {
 
 	@Autowired
-	private MockMvc mvc;
+	private MockMvc mockMvc;
 
 	@MockBean
 	private AdminService as;
 
+	@InjectMocks
 	private AdminController ac;
 
-	private String mockAdminJson = "{'adminId':0 ,'email':'a@a.net','firstName':'admin','lastName':'adminson'}";
+	private String mockAdminJson = "{\"adminId\":0 ,\"email\":\"a@a.net\",\"firstName\":\"admin\",\"lastName\":\"adminson\"}";
+	private String mockAdminJson2 = "{\"adminId\":0 ,\"email\":\"a2@a.net\",\"firstName\":\"admin\",\"lastName\":\"adminson\"}";
+	Admin admin0 = new Admin(0, "a@a.net", "admin", "adminson");
+	Admin admin2 = new Admin(0, "a2@a.net", "admin", "adminson");
+	
+	@Before
+	public void setUp() {
+		this.mockMvc = MockMvcBuilders.standaloneSetup(ac).build();
+	}
 
 	@Test
 	void testCreateNewAdmin() throws Exception {
+
+		Mockito.when(as.register(admin0)).thenReturn(true);
+		Mockito.when(as.register(admin2)).thenReturn(false);
+		this.mockMvc
+				.perform(post("/admin/new").contentType(MediaType.APPLICATION_JSON).content(mockAdminJson)
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isCreated())
+				.andExpect(content().string(containsString("User succesfully created!")));
 		
-		this.mvc.perform(post("/admin/new").content(mockAdminJson).accept(MediaType.APPLICATION_JSON))
-		.andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("User succesfully created!")));
 		
-//		mvc.perform( MockMvcRequestBuilders.post("admin/new")
-//			      .contentType(MediaType.APPLICATION_JSON).andExpect(MockMvcResultMatchers.status().isOk())
-//		            .andExpect(content().json(mockAdminJson));
+		this.mockMvc
+		.perform(post("/admin/new").contentType(MediaType.APPLICATION_JSON).content(mockAdminJson2)
+				.accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isConflict())
+		.andExpect(content().string(containsString("User creation failed!!")));
 
 	}
 
