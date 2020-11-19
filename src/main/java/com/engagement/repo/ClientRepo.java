842e0f5275dbb9119265ddcaedb4c062d43a18c9
enochcho <enochcho97@hotmail.com>
2020-11-19 12:37:35 -0500

    added javadocs to clientrepo

--- src/main/java/com/engagement/repo/ClientRepo.java
+++ src/main/java/com/engagement/repo/ClientRepo.java
@@ -8,6 +8,10 @@ import org.springframework.stereotype.Repository;
 
 import com.engagement.model.Client;
 
+/**
+ * This interface defines the methods to access the client table in the database
+ *
+ */
 @Repository
 public interface ClientRepo extends JpaRepository<Client, Integer> {
 	
