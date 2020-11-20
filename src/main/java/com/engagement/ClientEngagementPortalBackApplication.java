fda010b47f281bcfd43585638e099c7c6476861b
mcdonalddylan <soupaface@gmail.com>
2020-11-20 14:47:49 -0500

    encrypted the url username and password

--- src/main/java/com/engagement/ClientEngagementPortalBackApplication.java
+++ src/main/java/com/engagement/ClientEngagementPortalBackApplication.java
@@ -3,10 +3,14 @@ package com.engagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
+@EnableEncryptableProperties
 @SpringBootApplication
 public class ClientEngagementPortalBackApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(ClientEngagementPortalBackApplication.class, args);
 	}
 
---
+++ src/main/java/com/engagement/config/Config.java
@@ -0,0 +1,11 @@
+package com.engagement.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
+@Configuration
+@EnableEncryptableProperties
+public class Config {
+	
+}
