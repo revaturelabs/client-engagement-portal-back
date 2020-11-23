0b47bf61e4205b2c2543856d6508303ea85a71d5
brookewursten <brooke.wursten@gmail.com>
2020-11-23 07:44:37 -0800

    Added unit test for findByAdminId in the service-fixed

--- src/test/java/com/engagement/service/AdminServiceTest.java
+++ src/test/java/com/engagement/service/AdminServiceTest.java
@@ -1,10 +1,7 @@
 package com.engagement.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.doNothing;
 
@@ -32,30 +29,30 @@ import com.engagement.repo.caliber.TrainingClient;
 @ExtendWith(MockitoExtension.class)
 @ContextConfiguration
 class AdminServiceTest {
-	
+
 	@Mock
 	private AdminRepo ar;
-	
+
 	@Mock
 	private TrainingClient tc;
-	
+
 	@Mock
 	private ClientBatchRepo cbr;
-	
+
 	@Mock
 	private ClientRepo cr;
-	
+
 	@InjectMocks
 	private AdminService as;
-	
-	
+
+
 	/**
 	 * @author Brooke Wursten
 	 * tests the FindAllMappings method to be sure it gets the map we expect
 	 */
 	@Test
 	void testFindAllMappings() {
-		
+
 		/**
 		 * Set up mock return from client-batch repo
 		 */
@@ -66,15 +63,15 @@ class AdminServiceTest {
 		clientBatchList.add(new ClientBatch(2,"TR-102",client0));
 		clientBatchList.add(new ClientBatch(3,"TR-103",client1));
 		Mockito.when(cbr.findAll()).thenReturn(clientBatchList);
-		
+
 		Map<String,Integer> resultMap = new HashMap<String,Integer>(){{
 			put("TR-101",1);
 			put("TR-102",1);
 			put("TR-103",2);
-			}};
-		
+		}};
+
 		assertEquals((HashMap<String,Integer> )as.findAllMappings(), resultMap);
-		
+
 	}
 
 	/**
@@ -85,7 +82,7 @@ class AdminServiceTest {
 		Admin admin1= new Admin(1,"admin@rev.net","admin","adminson");
 
 		Mockito.when(ar.findByAdminId(1)).thenReturn(admin1);
-		assertEquals(admin1,ar.findByAdminId(1));
+		assertEquals(admin1,as.findByAdminId(1));
 	}
 
 
@@ -97,17 +94,17 @@ class AdminServiceTest {
 	@Test
 	void testFindAll() {
 		/**
-		*Set up mock from Admin repo 
-		*/
+		 *Set up mock from Admin repo 
+		 */
 		Admin admin1= new Admin(1,"a@b","fstubtest1","lstubtest1");
 		Admin admin2= new Admin(12,"a@b","fstubtest2","lstubtest2");
-		
+
 		List<Admin> admins = new ArrayList<Admin>();
-		
+
 		Mockito.when(ar.findAll()).thenReturn(admins);
 		assertEquals(admins,as.findAll());
 	}
-	
+
 	/**
 	 * @author daniel constantinescu
 	 * This unit test if  finding by email an admin works properly
@@ -115,15 +112,15 @@ class AdminServiceTest {
 	@Test
 	void testFindByEmailSucces() {
 		/**
-		*Set up mock from Admin repo 
-		*/
+		 *Set up mock from Admin repo 
+		 */
 		Admin admin1= new Admin(1,"a@b","fstubtest1","lstubtest1");
-		
+
 		Mockito.when(ar.findByEmail(admin1.getEmail())).thenReturn(admin1);
 		assertEquals(admin1,as.findByEmail(admin1.getEmail()));
-		
+
 	}
-	
+
 	/**
 	 * @author daniel constantinescu
 	 * This unit test the  case  for admin not found 
@@ -131,44 +128,44 @@ class AdminServiceTest {
 	@Test
 	void testFindByEmailFail() {
 		/**
-		*Set up mock from Admin repo 
-		*/
+		 *Set up mock from Admin repo 
+		 */
 		Admin admin1= new Admin(1,"a@b","fstubtest1","lstubtest1");
-		
+
 		Mockito.when(ar.findByEmail(admin1.getEmail())).thenReturn(null);
 		assertNull(as.findByEmail(admin1.getEmail()));
 	}	
 
 
-/**
- * @author daniel constantinescu
- * This unit test if  save properly Admin
- */
-@Test
-void testSaveSuccess() {
 	/**
-	*Set up mock from Admin repo 
-	*/
-	Admin admin1= new Admin(1,"a@b","fstubtest1","lstubtest1");
-	
-	Mockito.when(ar.save(admin1)).thenReturn(admin1);
-	assertTrue( as.save(admin1));
-}
+	 * @author daniel constantinescu
+	 * This unit test if  save properly Admin
+	 */
+	@Test
+	void testSaveSuccess() {
+		/**
+		 *Set up mock from Admin repo 
+		 */
+		Admin admin1= new Admin(1,"a@b","fstubtest1","lstubtest1");
 
-/**
- * @author daniel constantinescu
- * This unit test if  save properly Admin
- */
-@Test
-void testUpdateSuccess() {
+		Mockito.when(ar.save(admin1)).thenReturn(admin1);
+		assertTrue( as.save(admin1));
+	}
+
 	/**
-	*Set up mock from Admin repo 
-	*/
-	Admin admin1= new Admin(1,"a@b","fstubtest1","lstubtest1");
-	
-	Mockito.when(ar.save(admin1)).thenReturn(admin1);
-	assertEquals(admin1, as.update(admin1));
-}
+	 * @author daniel constantinescu
+	 * This unit test if  save properly Admin
+	 */
+	@Test
+	void testUpdateSuccess() {
+		/**
+		 *Set up mock from Admin repo 
+		 */
+		Admin admin1= new Admin(1,"a@b","fstubtest1","lstubtest1");
+
+		Mockito.when(ar.save(admin1)).thenReturn(admin1);
+		assertEquals(admin1, as.update(admin1));
+	}
 
 	/**
 	 * @author daniel constantinescu
@@ -176,36 +173,36 @@ void testUpdateSuccess() {
 	 * @param BatchId,email
 	 * @return true
 	 */
-	
+
 	@Test
 	void testMapBatchToClientSucces() {
 		String BatchId="ABC";
 		String email="a@b";
-		
+
 		Client c =  new Client (1,"a@b","company", "2488546789");
 		ClientBatch cb=new ClientBatch(1000,"ABC",c);
-		
+
 		Mockito.when(cr.findByEmail(email)).thenReturn(c);
 		Mockito.when(cbr.save(cb)).thenReturn(cb);
-	
+
 		assertTrue(as.MapBatchtoClient(BatchId,email));
 	}	
-	
+
 	/**
 	 * @author daniel constantinescu
 	 * test unmap batch from a client 
 	 * @param batchId,email
 	 * @return true
 	 */
-	
+
 	@Test
 	void testUnMapBatchFromClientSucces(){
 		String BatchId="ABC";
 		String email="a@b";
-		
+
 		Client c =  new Client (1,"a@b","company", "2488546789");
 		ClientBatch cb=new ClientBatch(1000,"ABC",c);
-		
+
 		Mockito.when(cbr.findByBatchId(BatchId)).thenReturn(cb);
 		doNothing().when(cbr).deleteByBatchId(BatchId);
 		assertTrue(as.UnMapBatchFromClient(BatchId, email));
