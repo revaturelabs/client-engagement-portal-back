5aad590ee249b2d503fc91440ece7ed3a18de3fc
Matthew Thomas <matthew.thomas@revature.net>
2020-11-23 13:08:57 -0600

    Update ClientController.java

--- src/main/java/com/engagement/controller/ClientController.java
+++ src/main/java/com/engagement/controller/ClientController.java
@@ -91,13 +91,13 @@ public class ClientController {
     }
 
 	/**
-	 * Find all information about a branch by the batch id
+	 * Find all information about a employee by the batch id
 	 * @param batchId: The identifier in Caliber to identify a batch
 	 * @return Returns the Batch associated with the batchId
 	 * @author Kelsey Iafrate
 
 	 */
-	@ApiOperation(value = "Returns All inforation about a bramch by given id.")
+	@ApiOperation(value = "Returns All inforation about a employee by given id.")
 	@GetMapping("/batch/{batchId}")
 	public Batch getBatchById(@PathVariable("batchId") String batchId) {
 		return cs.getBatchByBatchId(batchId);
