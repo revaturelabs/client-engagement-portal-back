db9a695d1abf8a9c917925a41712aca1245edd0c
TR-1000 <t.l.ross@outlook.com>
2021-01-15 16:51:49 -0600

    unit tests for the service layer

--- src/test/java/com/engagement/service/MessageServiceTest.java
+++ src/test/java/com/engagement/service/MessageServiceTest.java
@@ -1,19 +1,30 @@
 package com.engagement.service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ContextConfiguration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.engagement.model.Admin;
 import com.engagement.model.Client;
 import com.engagement.model.Message;
 import com.engagement.model.Message.haveBatch;
+import com.engagement.model.dto.MessageAdminDTO;
+import com.engagement.model.dto.MessageClientDTO;
 import com.engagement.repo.AdminRepo;
 import com.engagement.repo.ClientRepo;
 import com.engagement.repo.MessageRepo;
@@ -33,32 +44,104 @@ class MessageServiceTest {
 	private ClientRepo clientRepo;
 	
 	@InjectMocks
-	private MessageService messageSevice;
+	private MessageService messageService;
 	
-	@Mock
-	private Message message;
+	Client client0 = new Client(0, "client0@a.net", "walmart", "573-555-3535");
+	Admin admin0= new Admin(0,"admin0@b","firstnameadmin0","lastnameadmin0");
 	
-	Client client0 = new Client(0, "c0@a.net", "walmart", "573-555-3535");
+	MessageAdminDTO messageAdminDTO = new MessageAdminDTO(client0.getClientId(), admin0.getAdminId(), "Hello from MessageAdminDTO");
+	MessageClientDTO messageClientDTO = new MessageClientDTO(admin0.getAdminId(), client0.getClientId(), "Hello from MessageClientDTO");
 	
-	Client client1 = new Client(1, "c1@a.net", "bankone", "573-555-4535");
-
-	Admin admin1= new Admin(1,"a@b","fstubtest1","lstubtest1");
+	Message testMessage = new Message(0, true, admin0, client0, "Test message", null, false);
 	
-	Admin admin2= new Admin(3,"a@b","fstubtest1","lstubtest1");
+	List<Message> messages = new ArrayList<>();
 	
+	
+	/**
+	 * This tests verifies that the messageService.addMessageAdmin method calls the save method.
+	 * 
+	 * @author Takia Ross
+	 */
 	@Test
-	void testAddMessages() {
+	void testAddMessageAdmin() {
 		// Create a message
-		Message mockMessage0 = new Message(0, false, null, client0, "Hello", date, false, haveBatch.NOBATCH);
-		Mockito.when(messageRepo.save(mockMessage0)).thenReturn(mockMessage0);
-		
-		//Message mockMessage1 = new Message(1, false, null, client1, "Hello, World", date, false, haveBatch.NOBATCH);
+		Admin admin = adminRepo.findByAdminId(messageAdminDTO.getAdminId());
+		Client client = clientRepo.findById(messageAdminDTO.getClientId());
+		Message mockMessageAdmin = new Message(0, true, admin, client, messageAdminDTO.getMessage(), null, false);
+		Mockito.when(messageRepo.save(mockMessageAdmin)).thenReturn(mockMessageAdmin);
+		messages.add(mockMessageAdmin);
 		
 		// Save Message
+		Message newAdminMessage = messageService.addMessageAdmin(messageAdminDTO);
+		
+		// Verify save
+		assertEquals("Hello from MessageAdminDTO", newAdminMessage.getMessage());
+		
 	}
 	
 	
+	/**
+	 * This tests verifies that the messageService.addMessageClient method calls the save method.
+	 * 
+	 * @author Takia Ross
+	 */
+	@Test
+	void testAddMessageClient() {
+		// Create a message
+		Admin admin = adminRepo.findByAdminId(messageClientDTO.getAdminId());
+		Client client = clientRepo.findById(messageClientDTO.getClientId());
+		Message mockClientMessage = new Message(0, false, admin, client, messageClientDTO.getMessage(), null, false);
+		Mockito.when(messageRepo.save(mockClientMessage)).thenReturn(mockClientMessage);
+		messages.add(mockClientMessage);
+		
+		// Save Message
+		Message newClientMessage = messageService.addMessageClient(messageClientDTO);
+		
+		// Verify save
+		assertEquals("Hello from MessageClientDTO", newClientMessage.getMessage());
+	}
 	
 	
-
+	/**
+	 * This tests verifies that the findAll() method has been called.
+	 * 
+	 * @author Takia Ross
+	 */
+	@Test
+	void testGetMessages() {
+		Mockito.when(messageRepo.findAll()).thenReturn(messages);
+		assertNotNull(messageService.getMessages());
+		
+	}
+	
+	
+	/**
+	 * This tests verifies findById(int id) method has been called.
+	 * 
+	 * @author Takia Ross
+	 */
+	@Test
+	void testGetMessageById() {
+		Mockito.when(messageRepo.findById(0)).thenReturn(testMessage);
+		Message foundMessage = messageService.getMessageById(0);
+		assertEquals("Test message", foundMessage.getMessage());
+	}
+	
+	
+	/**
+	 * This tests verifies delete method is called.
+	 * 
+	 * @author Takia Ross
+	 */
+	@Test
+	void testDeleteMessage() {
+		// if message exists
+		Mockito.when(messageRepo.findById(0)).thenReturn(testMessage);
+		assertEquals(messageService.deleteMessage(0), "Message id: " + 0 + " has successfully deleted");
+		// if message doesn't exist
+		Mockito.when(messageRepo.findById(1)).thenReturn(null);
+		assertEquals(messageService.deleteMessage(1), "Message NOT found");
+	}
+	
+	
 }
