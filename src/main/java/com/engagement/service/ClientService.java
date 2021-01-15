54dc71d769df9aa16e32a304661323178127052c
Cory Sebastian <sebastiancory@yahoo.com>
2021-01-15 18:21:39 -0500

    Added ability to get full batch information for a client by providing the client's email

--- src/main/java/com/engagement/service/ClientService.java
+++ src/main/java/com/engagement/service/ClientService.java
@@ -1,5 +1,6 @@
 package com.engagement.service;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -143,21 +144,35 @@ public class ClientService {
 		 * @return List of Batchoverview DTO to show batches mapped to client and brief overview
 		 */
 		public List<BatchOverview> getBatchInfoByEmail(String email) {
-					List<BatchOverview> results = new LinkedList<>();
-					Client client = cr.findByEmail(email);
-					List<ClientBatch> batches = cbr.findByClient(client);
-					Batch batchesapi;
-					
-				
-					
-					for(int i = 0; i < batches.size(); i++)
-					{
-						batchesapi = bc.getBatchById(batches.get(i).getBatchId());
-						results.add(new BatchOverview(batchesapi.getBatchId(), batchesapi.getName(), batchesapi.getSkill()));
-					}
-						
-					return results;
-				}	
-		
-		
+			List<BatchOverview> results = new LinkedList<>();
+			Client client = cr.findByEmail(email);
+			List<ClientBatch> batches = cbr.findByClient(client);
+			Batch batchesapi;
+
+
+
+			for(int i = 0; i < batches.size(); i++)
+			{
+				batchesapi = bc.getBatchById(batches.get(i).getBatchId());
+				results.add(new BatchOverview(batchesapi.getBatchId(), batchesapi.getName(), batchesapi.getSkill()));
+			}
+
+			return results;
+		}
+
+	/**
+	 * Returns full batch information for a client with email email
+	 * @param email the client's email
+	 * @return a list of all the batches and their information mapped the client
+	 * @author Cory Sebastian
+	 */
+		public List<Batch> getAllBatchInfoByEmail(String email) {
+			List<Batch> batchInfo = new ArrayList<>();
+			List<BatchOverview> batchOverviews = getBatchInfoByEmail(email);
+
+			for (BatchOverview batchOverview : batchOverviews) {
+				batchInfo.add(bc.getBatchById(batchOverview.getBatchId()));
+			}
+			return batchInfo;
+		}
 }
