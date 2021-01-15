d143e0adbe72e16c3aad3179ade8edf273cbe1ad
= <jadenwilson717@gmail.com>
2021-01-15 13:25:14 -0700

    added initial db data for in memory h2 db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO admin
+(email, first_name, last_name)
+VALUES
+('test@test.com', 'Test', 'Tester');
+
+INSERT INTO client
+(company_name, email, phone_number)
+VALUES
+('Test Company LLC', 'client@client.com', '123-456-7890');
