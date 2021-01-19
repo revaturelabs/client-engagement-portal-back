f908cb358e38cd598c2a09828db84a39d05f6ab8
tkmoney11 <tylerkim@outlook.com>
2021-01-18 21:07:56 -0800

    pushing rebased branch for message-page merge

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
 insert into admin(admin_id, email, first_name,last_name) values(1, 'tdeng.tester.notification@gmail.com', 'Sam', 'Smith');
 insert into admin(admin_id, email, first_name,last_name) values(2, 'tdeng.tester2.notification@gmail.com', 'Tian', 'D');
 insert into admin(admin_id, email, first_name,last_name) values(4, 'admin2@admin', 'Sam', 'Smith');
-insert into client(client_id,company_name, email, phone_number) values (1,'Walmart', 'client2@Walmart', '123-456-7890');
+insert into client(client_id,company_name, email, phone_number) values (1,'Walmart', 'client@client.com', '123-456-7890');
+INSERT INTO ADMIN (email, first_name, last_name) values ('test@test.com', 'Sam', 'Smith');
