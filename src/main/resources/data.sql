5d8218290e58c33ddb17d469b22785b0948a37cc
= <jadenwilson717@gmail.com>
2021-01-20 19:08:03 -0700

    added test user for email test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,8 @@ INSERT INTO client
 (company_name, email, phone_number)
 VALUES
 ('Test Company LLC', 'client@client.com', '123-456-7890');
+
+INSERT INTO admin
+(email, first_name, last_name)
+VALUES
+('daniel.monteton@gmail.com', 'Daniel', 'Mont-Eton');
